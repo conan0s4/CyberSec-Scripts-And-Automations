3fa9cf765e8a9f7d8bf18fd0bda07c6851338f10
Alexander Sapo <alexandermsapo@gmail.com>
2026-02-17 17:55:01 +0800

    practice 01

--- web scraper.py
+++ web scraper.py
@@ -1,1 +1,28 @@
 #start
+from bs4 import BeautifulSoup
+import requests
+
+# https://webscraper.io/test-sites
+url = input("enter url: ")
+
+x = requests.get(url)
+
+
+soup = BeautifulSoup(x.content, "html.parser")
+
+
+res = soup.find_all('span')
+print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
