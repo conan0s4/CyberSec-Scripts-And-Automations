d78e80d15fc2b35bc0b31271700f5bb6caf4c07d
Alexander Sapo <alexandermsapo@gmail.com>
2026-02-24 14:36:07 +0800

    net user included

--- enumerate-startup-program.py
+++ enumerate-startup-program.py
@@ -1,11 +1,14 @@
 import subprocess
 
+
+Res = subprocess.run(["net" , "user"])
+
 #startup for all users
 res = subprocess.run(["dir" , r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"] ,shell=True) #if built in terminal not exe
 print(res)
 
 #all user startup registry
-#if exe seperate args
+#seperate args in "" ,
 #r raw string for file paths or double \\
 r3s = subprocess.run(["reg" ,"query" ,r"HKLM\Software\Microsoft\Windows\CurrentVersion\Run"])
 print(r3s)
