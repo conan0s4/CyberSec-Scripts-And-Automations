6e0801346da05bb9e39f97641fa7ae365043c99a
Alexander Sapo <alexandermsapo@gmail.com>
2026-03-25 10:50:24 +0800

    sub-domain recon script initial

---
+++ Introduction to Python for Offensive Security-RedTeamLeaders/Info Gathering and OSINT Scripts/crtsh_subs.py
@@ -0,0 +1,36 @@
+'''''''''
+
+Passive-Only Recon Write crtsh_subs.py that queries 
+https://crt.sh/?q=%.example.com&output=json and dumps unique sub-domains.
+
+references:
+https://www.python-httpx.org/
+https://hackdb.com/item/crtsh
+
+wildcard search query
+Find All Subdomains: %.target.com (e.g., %.google.com) returns all subdomains, excluding the apex domain itself.
+
+
+status: not done 
+'''''''''
+
+import httpx
+
+crt_web = "https://crt.sh/"
+web_target = input("input target: ")
+query = {'q':f"%.{web_target}","output":"json"}
+
+with httpx.Client() as client:
+    r = httpx.get(crt_web, params=query)
+    data = r.json()
+    print(data)
+
+
+
+
+
+
+
+
+
+
