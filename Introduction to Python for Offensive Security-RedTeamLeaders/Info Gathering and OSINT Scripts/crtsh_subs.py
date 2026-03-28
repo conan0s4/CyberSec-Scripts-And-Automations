13ea26538975b1ec720dadad349894ee015518ab
Alexander Sapo <alexandermsapo@gmail.com>
2026-03-28 20:59:09 +0800

    Update crtsh_subs.py

--- Introduction to Python for Offensive Security-RedTeamLeaders/Info Gathering and OSINT Scripts/crtsh_subs.py
+++ Introduction to Python for Offensive Security-RedTeamLeaders/Info Gathering and OSINT Scripts/crtsh_subs.py
@@ -11,7 +11,6 @@ wildcard search query
 Find All Subdomains: %.target.com (e.g., %.google.com) returns all subdomains, excluding the apex domain itself.
 
 
-status: not done 
 '''''''''
 
 import httpx
@@ -21,21 +20,32 @@ crt_web = "https://crt.sh/"
 web_target = input("input target: ")
 query = {'q':f"%.{web_target}","output":"json"}
 
-with httpx.Client() as client:
-    r = httpx.get(crt_web, params=query)
-    data = r.json()
-    print(data)
-    print("""
-    -------------------------------------------------------------------------------
-                                    Sub-Domains:
-    -------------------------------------------------------------------------------
-    """)
 
+
+
+
+with httpx.Client() as client:
+    r = httpx.get(crt_web, params=query , timeout=60.0)
+    data = r.json()
+
+
+
+
+print("""
+-----------------------------------------------------------
+sub-domains: 
+-----------------------------------------------------------
+""")
+
+domains = []
+d = []
 
 for item in data:
-    print(item["common_name"])
-
-
+    domain = item.get("common_name")
+    if domain not in domains:
+        domains.append(domain)
+        d.append(domain)
+        print(domain)
 
 
 
