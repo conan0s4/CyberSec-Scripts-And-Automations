fedd964aa6a4cbaae44f05d7a6ea3e3342dd686f
Alexander Sapo <alexandermsapo@gmail.com>
2026-04-17 13:57:39 +0800

    update

--- Introduction to Python for Offensive Security-RedTeamLeaders/Info Gathering and OSINT Scripts/crtsh_subs.py
+++ Introduction to Python for Offensive Security-RedTeamLeaders/Info Gathering and OSINT Scripts/crtsh_subs.py
@@ -1,49 +1,51 @@
-'''''''''
-Passive-Only Recon Write crtsh_subs.py that queries 
-https://crt.sh/?q=%.example.com&output=json and dumps unique sub-domains.
-references:
-https://www.python-httpx.org/
-https://hackdb.com/item/crtsh
-wildcard search query
-Find All Subdomains: %.target.com (e.g., %.google.com) returns all subdomains, excluding the apex domain itself.
-'''''''''''
-
 import httpx
 import ssl
 import socket
 # note: implement banner_grab() must use both domains we got from enum and probe
 # enum-->probe-->banner grab
+#implementation of argparse
 def main():
     web_target = input("web target: ")
-    recon = input("enum/probe?:")
-    if recon == "enum":
-        dns_enumerate(web_target)
-    elif recon == "probe":
-        dns_probe(web_target)
+    res = input("enum/probe/exit?:")
+    Domains = []
+    e_domains = []
+    p_domains = []
+    if res == "enum":
+        dns_enumerate(web_target,e_domains)
+    elif res == "probe":
+        dns_probe(web_target,p_domains)
 
-def dns_enumerate(web_target):
+    Domains = e_domains + p_domains
+    print(Domains)
+
+def dns_enumerate(web_target,e_domains):
     crt_web = "https://crt.sh/"
     query = {'q': f"%.{web_target}", "output": "json"}
     with httpx.Client() as client:
-        r = httpx.get(crt_web, params=query, timeout=10.0)
+        r = httpx.get(crt_web, params=query, timeout=60.0)
         data = r.json()
-    domains = []
-    d = []
     for item in data:
         domain = item.get("name_value")
-        if domain not in domains:
-            domains.append(domain)
-            d.append(domain)
-            print(domain)
+        if domain not in e_domains:
+            e_domains.append(domain)
+#    for i in range(len(e_domains)):
+#        print(e_domains[i])
 
-def dns_probe(web_target):
+def dns_probe(web_target,p_domains):
     hostname =  web_target
     context = ssl.create_default_context()
     with socket.create_connection((hostname, 443)) as sock:
         with context.wrap_socket(sock, server_hostname=hostname) as ssock:
             cert = ssock.getpeercert()
-    print(cert["subject"])
-    print(cert["subjectAltName"])
+    dom0 = list(cert["subject"][4])
+    dom1 = list(cert["subjectAltName"])
+    for item in range(len(dom1)):
+        p_domains.append(dom1[item][1])
+    p_domains.append(dom0[0][1])
+#    print(p_domains)
+
+#def banner_grab():
+
 
 if __name__ == "__main__":
     main()
